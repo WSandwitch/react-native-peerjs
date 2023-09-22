babb2b16e8b3e5accd25e229b4fcd60d69b7d728
Denis Yarikov <yarikov@niisi.ras.ru>
2023-09-22 15:12:48 +0300

    update

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -e
 
 exec 2>&1
 
-rm -rf dist/ peerjs/
-git clone https://github.com/peers/peerjs peerjs
+#rm -rf dist/ peerjs/
+#git clone https://github.com/peers/peerjs peerjs
 cd peerjs
-git apply ../decoupling.diff
-../node_modules/.bin/parcel build --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
+#git checkout v1.4.7
+#git apply ../decoupling.diff
+../node_modules/.bin/parcel build --no-minify --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
 cd ../
 cat imports.js dist/peerjs.min.js > dist/react-native-peerjs.js
 rm dist/peerjs.min.js
